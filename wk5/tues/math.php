c3e320c470dc900b4648ba229d1337e55f695a50
Prof Blanc <ben.blanc@georgebrown.ca>
2026-02-03 08:53:54 -0500

    Class Code Feb 3, 2026 @ 8h55

--- wk5/tues/math.php
+++ wk5/tues/math.php
@@ -13,3 +13,32 @@ Create an output div to store the result of calculation
     only allow 4 operators are choices for operator
     calculate result (using if or switch statements)
 -->
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form id="myForm">
+        <legend>Number 1</legend>
+        <input type="number" name="n1" value=""/>
+        <br/><br/>
+        <legend>Operator</legend>
+        <select name="operator">
+            <option value="+">+ (Add)</option>
+            <option value="-">- (Substract)</option>
+            <option value="*">* (Multiply)</option>
+            <option value="/">/ (Divide)</option>
+        </select>
+        <br/><br/>
+        <legend>Number 2</legend>
+        <input type="number" name="n2" value=""/>
+        <br/><br/>
+        <input type="submit" name="submit" value="Submit">
+        <div id="output"></div>
+        
+
+</form>
+</body>
+</html>    
