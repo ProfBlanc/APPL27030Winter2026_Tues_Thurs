0f756a589abdd2d2e08ab54e296e4f94a345c0d7
Prof Blanc <ben.blanc@georgebrown.ca>
2026-02-03 08:46:40 -0500

    Class Code Feb 3, 2026 @ 8h45

--- wk5/tues/counting.php
+++ wk5/tues/counting.php
@@ -10,36 +10,79 @@ Once user clicks on submit button, php will
     - validate input
     - output the result to screen in div with id output
 -->
+
+<?php
+$title = "Our Counting App";  //superglobal variables => build-in php vars that start with "$_" and are all caps
+
+//print_r($_GET);
+
+function manually_validate_int($data){
+
+    if(is_numeric($data)){
+        return intval($data);
+    }
+    return 1;
+}
+
+$output = "";
+$step = $end = $start = "";
+if(isset($_GET['submit'])){
+
+  $start = $_GET['start'];
+  $end = $_GET['end'];
+  $step = $_GET['step'];
+
+  //validate the data
+  $start = manually_validate_int($start);
+  $end = manually_validate_int($end);
+  $step = manually_validate_int($step);
+
+  if($end < $start)
+    $end = $start + 1;
+
+  if($step < 1)
+    $step = 1;
+
+  $output .= "Counting from $start to $end, incrementing by $step<br/>";
+
+  for($i = $start; $i<= $end; $i+= $step){
+    $output .= "$i<br/>";
+  }
+
+}
+
+?>
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title><?=$title;?></title>
 </head>
 <body>
 <form id='myForm' method='get' >
 <legend>
     Starting Number
 </legend>
-<input type="number" name="start" min=1 max=100 />
+<input type="number" name="start" value="<?=$start;?>" min=1 max=100 />
 <br/>
 <br/>
 <legend>
     Ending Number
 </legend>
-<input type="number" name="end" min=1 max=100 />
+<input type="number" name="end" value="<?=$end;?>" min=1 max=100 />
 <br/>
 <br/>
 <legend>
     Stepping Number
 </legend>
-<input type="number" name="step" min=1 max=5 />
+<input type="number" name="step" value="<?=$step;?>" min=1 max=5 />
 <br/>
 <br/>
 <input type="submit" name="submit" value="Submit" />
 </form>
     
-<div id="output"></div>
+<div id="output"><?=$output;?></div>
 
 </body>
 </html>
---
+++ wk5/tues/math.php
@@ -0,0 +1,15 @@
+<!--
+Create a form with fields
+    Num 1 input type of number
+    Operator: either +, -, *, / (select & option)
+    Num 2: input type of number
+    submit button
+
+Create an output div to store the result of calculation
+    output to be "num1 Operator num2 = result"
+
+    Using PHP code, determine if user submitted the form
+    validate nums as numerical input
+    only allow 4 operators are choices for operator
+    calculate result (using if or switch statements)
+-->
